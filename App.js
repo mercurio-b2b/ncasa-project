4f99ead9e8fab68ed5e041fd6b209f568c284731
mercurio-b2b <stefan.thomas@idaplan.com>
2020-05-22 18:28:59 -0500

    change method to mp4 loop

--- App.js
+++ App.js
@@ -1,50 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Animated, Image, Easing } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const backgroundImage = require('./assets/icon.png')
+import { Video } from 'expo-av';
 
 export default class App extends Component {
 
 	constructor(props) { 
     	super(props);
-    	this.animatedValue = new Animated.Value(0)
-    	this.handleAnimation = this.handleAnimation.bind(this);
 	}
 
-	componentDidMount() {
-        this.handleAnimation();
-  	}
-
-	handleAnimation = () => {
-		console.log("Animation start");
-	    // A loop is needed for continuous animation
-	    Animated.loop(
-	      // Animation consists of a sequence of steps
-	      Animated.sequence([
-	        // start rotation in one direction (only half the time is needed)
-	        Animated.timing(this.animatedValue, {toValue: 1.0, duration: 150, easing: Easing.linear, useNativeDriver: true}),
-	        // rotate in other direction, to minimum value (= twice the duration of above)
-	        Animated.timing(this.animatedValue, {toValue: -1.0, duration: 300, easing: Easing.linear, useNativeDriver: true}),
-	        // return to begin position
-	        Animated.timing(this.animatedValue, {toValue: 0.0, duration: 150, easing: Easing.linear, useNativeDriver: true})
-	      ])
-	    ).start(); 
-  	}
-  
 	render(){
 	  return (
 	    <View style={styles.container}>
-	      <Animated.Image
-			  source={backgroundImage}
-			  resizeMode='contain'
-			  style={{
-			    transform: [{
-			      rotate: this.animatedValue.interpolate({
-			        inputRange: [-1, 1],
-			        outputRange: ['-0.1rad', '0.1rad']
-			      })
-			    }]
-			  }}
+	      <Video
+			  source={ require('./assets/animated-video.mp4') }
+			  rate={1.0}
+			  volume={1.0}
+			  isMuted={false}
+			  resizeMode="cover"
+			  shouldPlay
+			  isLooping
+			  style={{ width: 314, height: 202 }}
 			/>
 	    </View>
 	  );
